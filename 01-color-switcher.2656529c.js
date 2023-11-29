252106870032f4d4aa23506de3d81b30ab455d42
TatyanaZdota <99191661+TatyanaZdota@users.noreply.github.com>
2023-11-29 10:00:56 +0000

    Deploying to gh-pages from @ TatyanaZdota/goit-js-hw-09@99deaacd464f603b145748a838cb7947661ff8b4 🚀

--- 01-color-switcher.2656529c.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=null;t.addEventListener("click",(function(e){n=setInterval((function(){document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0)),t.disabled=!0}),1e3)})),e.addEventListener("click",(function(e){clearInterval(n),t.disabled=!1}))}();
-//# sourceMappingURL=01-color-switcher.2656529c.js.map
---
+++ 01-color-switcher.552bacec.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=null;t.addEventListener("click",(function(n){a=setInterval((function(){document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0)),t.disabled=!0,e.disabled=!1}),1e3)})),e.addEventListener("click",(function(n){clearInterval(a),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.552bacec.js.map
